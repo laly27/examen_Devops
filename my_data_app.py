a99e2525ff721f3b1c5d99c352c9c570092d3753
fatoumata <fatoubatoma27@gmail.com>
2025-06-18 00:17:21 +0200

    rename

--- my_data_app.py
+++ my_data_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flaskgit_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
